14c75848a678cea917f5180c0536db2249aed510
Austin Luu <austinowenluu@gmail.com>
2019-04-04 23:15:20 -0400

    Update Week8_Example1_Line_following_based_frame_onlyD2L.ino

--- Week8_Example1_Line_following_based_frame_onlyD2L/Week8_Example1_Line_following_based_frame_onlyD2L.ino
+++ Week8_Example1_Line_following_based_frame_onlyD2L/Week8_Example1_Line_following_based_frame_onlyD2L.ino
@@ -4,10 +4,15 @@
 #include <USART.h>
 
 uint16_t Line1, Line2, Line3, Line4, Dis_left, Dis_center, Dis_right;
-uint16_t t = 1000, threshold_table = 650,  threshold_blackTape = 850, threshold_wall = 300, counter = 0;
+uint16_t threshold_table = 650,  threshold_blackTape = 850, threshold_wall = 300, counter = 0;
 int left_en_total = 0, right_en_total = 0, base_speed = 550;
 int left_en_cur, right_en_cur, left_en_pre, right_en_pre;
 int c, e, r = 0, p = 10;
+
+/////////////////////////
+/////////////////////////
+/////////////////////////
+/////////////////////////
 
 void straight (int dist){
 DDRC = DDRC & (~((1 << DDRC2) | (1 << DDRC3))); //can also be DDRC&=~~((1<<DDRC2)|(1<<DDRC3)), Week 5 note Page 30
@@ -23,32 +28,38 @@ motor(base_speed, base_speed);
 
 while (i)//drive code
 { 
-    left_en_cur = PINC & (1 << PINC2);
-    right_en_cur = PINC & (1 << PINC3);
-    c = left_en_total - right_en_total;
-    e = r - c;
 
-    if (left_en_cur != left_en_pre){
-      left_en_total++;
-      left_en_pre = left_en_cur;
-    }
+  left_en_cur = PINC & (1 << PINC2);
+  right_en_cur = PINC & (1 << PINC3);
+  c = left_en_total - right_en_total;
+  e = r - c;
 
-    if (right_en_cur != right_en_pre){ 
-      right_en_total++;
-      right_en_pre = right_en_cur;
-    }
+  if (left_en_cur != left_en_pre){
+    left_en_total++;
+    left_en_pre = left_en_cur;
+  }
 
-    if ((left_en_total >= dist) && (right_en_total >= dist)){ 
-      motor(0, 0); 
-      break;
-    }
-    
-    if (e != 0){
-      motor(base_speed-e*p, base_speed+e*p)
-    }
+  if (right_en_cur != right_en_pre){ 
+    right_en_total++;
+    right_en_pre = right_en_cur;
+  }
+
+  if ((left_en_total >= dist) && (right_en_total >= dist)){ 
+    motor(0, 0); 
+    break;
+  }
+  
+  if (e != 0){
+    motor(base_speed-e*p, base_speed+e*p)
+  }
 
   }
 }
+
+/////////////////////////
+/////////////////////////
+/////////////////////////
+/////////////////////////
 
 void turn (int dir)//left = 0 right = 1
 {
@@ -72,33 +83,38 @@ while (1){
   left_en_cur = PINC & (1 << PINC2);
   right_en_cur = PINC & (1 << PINC3);
 
-    if (left_en_cur != left_en_pre)
-      { left_en_total++;
-        left_en_pre = left_en_cur;
-      }
+  if (left_en_cur != left_en_pre)
+    { left_en_total++;
+      left_en_pre = left_en_cur;
+    }
 
-      if (right_en_cur != right_en_pre)
-        { right_en_total++;
-          right_en_pre = right_en_cur;
-        }
+  if (right_en_cur != right_en_pre)
+    { right_en_total++;
+      right_en_pre = right_en_cur;
+    }
 
-        if ((left_en_total >= 16) && (right_en_total >= 16))
-          { 
-            motor(0, 0); 
-            break;
-          }
-          if ((left_en_total >= 16) && (right_en_total < 16))
-          {
-            motor(0, base_speed);
-          }
+  if ((left_en_total >= 16) && (right_en_total >= 16))
+    { 
+      motor(0, 0); 
+      break;
+    }
+    if ((left_en_total >= 16) && (right_en_total < 16))
+    {
+      motor(0, base_speed);
+    }
 
-          if ((left_en_total < 16) && (right_en_total >= 16))
-          {
-            motor(base_speed,0);
-          }
-        }
+    if ((left_en_total < 16) && (right_en_total >= 16))
+    {
+      motor(base_speed,0);
+    }
+  }
 
-      }
+}
+
+/////////////////////////
+/////////////////////////
+/////////////////////////
+/////////////////////////
 
 int main(void)
 {
@@ -142,6 +158,8 @@ int main(void)
   straight(58);
   turn(1);
   straight(126);
+  lcdPrint( "YEEEEETTTTT");
+  delay_ms(1000);
   return 0;
 
 }
